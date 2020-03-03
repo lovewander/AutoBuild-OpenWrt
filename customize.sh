fa460c81fd85fdce1908c04e2c981df7751f8303
lovewander <33365049+lovewander@users.noreply.github.com>
2020-03-04 01:08:57 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,5 +5,23 @@
 # Author: eSirPlayground
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
+
+##################################
+# Custom feed
+##################################
+# add lienol feed: such like passwall and themes
+echo ""
+echo "Adding lienol packages feed"
+echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
+
+echo ""
+echo "Updating feeds"
+./scripts/feeds update -a
+
+echo ""
+echo "Installing feeds"
+./scripts/feeds install -a
+
+
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
